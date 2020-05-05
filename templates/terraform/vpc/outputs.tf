a8e3be6efcb98c4a7181cfc4468ea01919e8e629
Brice Carpentier <brice@daknet.org>
2020-05-05 13:30:13 +0200

    comment vpc module outputs

--- templates/terraform/vpc/outputs.tf
+++ templates/terraform/vpc/outputs.tf
@@ -1,13 +1,16 @@
+# will be used by both the eks and db modules
 output "vpc_id" {
   description = "VPC id"
   value       = module.vpc.vpc_id
 }
 
+# will be used by both the eks and db modules
 output "private_subnets" {
   description = "Private subnets"
   value = module.vpc.private_subnets
 }
 
+# will be used by the eks module
 output "cluster_name" {
   description = "Cluster name"
   value = local.cluster_name
